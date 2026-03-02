8f8198222ce353cf998888fe0a3560b1a048187f
SnowFox4827 <dwplocek@outlook.com>
2026-03-02 15:21:41 -0500

    added task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -39,10 +39,23 @@ public_users.get('/author/:author', function (req, res) {
   }
 });
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Task 4
+public_users.get('/title/:title', function (req, res) {
+  const title = req.params.title;
+  const bookKeys = Object.keys(books);
+  const matchedBooks = [];
+
+  bookKeys.forEach(key => {
+    if (books[key].title === title) {
+      matchedBooks.push(books[key]);
+    }
+  });
+
+  if (matchedBooks.length > 0) {
+    return res.status(200).json(matchedBooks);
+  } else {
+    return res.status(404).json({ message: "No books found with this title" });
+  }
 });
 
 // Get all books based on title
